1c976a14cd1ddd8ab706c39653d7be8be9e147fb
devasMartins-Tmd <dev.as.martins3@gmail.com>
2023-10-20 13:38:38 +0100

    API

--- config/db_connect.ts
+++ config/db_connect.ts
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 import color from 'colors';
 import { uri } from './uri';
+import { configDotenv } from 'dotenv';
+configDotenv({ path: './config/.env' });
 
 export const connectToDB = async () => {
+   console.log(process.env.MONGOURI, uri);
    try {
       mongoose.set('strictQuery', true);
-      const conn = await mongoose.connect(process.env.MONGOURI || uri);
+      const conn = await mongoose.connect(uri);
       console.log(color.cyan(`Connected to mongoDB: ${conn.connection.host}`));
    } catch (e) {
       console.log(color.cyan(`Error connecting to mongoDB`));
